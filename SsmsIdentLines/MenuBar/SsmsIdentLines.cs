8c195f89a515949cf232321d22a9beb63e9c8fa7
Christian Groothoff <ruffo324@gmail.com>
2018-03-22 16:37:55 +0100

    #1 ReSharper Cleanup code

--- SsmsIdentLines/MenuBar/SsmsIdentLines.cs
+++ SsmsIdentLines/MenuBar/SsmsIdentLines.cs
@@ -7,67 +7,58 @@ using Microsoft.VisualStudio.Shell.Interop;
 namespace SsmsIdentLines.MenuBar
 {
     /// <summary>
-    /// Command handler
+    ///     Command handler
     /// </summary>
     internal sealed class SsmsIdentLines
     {
         /// <summary>
-        /// Command ID.
+        ///     Command ID.
         /// </summary>
         public const int CommandId = 0x0100;
 
         /// <summary>
-        /// Command menu group (command set GUID).
+        ///     Command menu group (command set GUID).
         /// </summary>
         public static readonly Guid CommandSet = new Guid("b473d3e8-7a17-41e6-8508-ad0b1fe721e7");
 
         /// <summary>
-        /// VS Package that provides this command, not null.
+        ///     VS Package that provides this command, not null.
         /// </summary>
         private readonly Package package;
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="SsmsIdentLines"/> class.
-        /// Adds our command handlers for menu (commands must exist in the command table file)
+        ///     Initializes a new instance of the <see cref="SsmsIdentLines" /> class.
+        ///     Adds our command handlers for menu (commands must exist in the command table file)
         /// </summary>
         /// <param name="package">Owner package, not null.</param>
         private SsmsIdentLines(Package package)
         {
-            if (package == null)
-            {
-                throw new ArgumentNullException("package");
-            }
+            if (package == null) throw new ArgumentNullException("package");
 
             this.package = package;
 
-            OleMenuCommandService commandService = this.ServiceProvider.GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
+            OleMenuCommandService commandService =
+                ServiceProvider.GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
             if (commandService != null)
             {
                 var menuCommandID = new CommandID(CommandSet, CommandId);
-                var menuItem = new MenuCommand(this.MenuItemCallback, menuCommandID);
+                var menuItem = new MenuCommand(MenuItemCallback, menuCommandID);
                 commandService.AddCommand(menuItem);
             }
         }
 
         /// <summary>
-        /// Gets the instance of the command.
+        ///     Gets the instance of the command.
         /// </summary>
-        public static SsmsIdentLines Instance {
-            get;
-            private set;
-        }
+        public static SsmsIdentLines Instance { get; private set; }
 
         /// <summary>
-        /// Gets the service provider from the owner package.
+        ///     Gets the service provider from the owner package.
         /// </summary>
-        private IServiceProvider ServiceProvider {
-            get {
-                return this.package;
-            }
-        }
+        private IServiceProvider ServiceProvider => package;
 
         /// <summary>
-        /// Initializes the singleton instance of the command.
+        ///     Initializes the singleton instance of the command.
         /// </summary>
         /// <param name="package">Owner package, not null.</param>
         public static void Initialize(Package package)
@@ -76,20 +67,21 @@ namespace SsmsIdentLines.MenuBar
         }
 
         /// <summary>
-        /// This function is the callback used to execute the command when the menu item is clicked.
-        /// See the constructor to see how the menu item is associated with this function using
-        /// OleMenuCommandService service and MenuCommand class.
+        ///     This function is the callback used to execute the command when the menu item is clicked.
+        ///     See the constructor to see how the menu item is associated with this function using
+        ///     OleMenuCommandService service and MenuCommand class.
         /// </summary>
         /// <param name="sender">Event sender.</param>
         /// <param name="e">Event args.</param>
         private void MenuItemCallback(object sender, EventArgs e)
         {
-            string message = string.Format(CultureInfo.CurrentCulture, "Inside {0}.MenuItemCallback()", this.GetType().FullName);
+            string message = string.Format(CultureInfo.CurrentCulture, "Inside {0}.MenuItemCallback()",
+                GetType().FullName);
             string title = "SsmsIdentLines";
 
             // Show a message box to prove we were here
             VsShellUtilities.ShowMessageBox(
-                this.ServiceProvider,
+                ServiceProvider,
                 message,
                 title,
                 OLEMSGICON.OLEMSGICON_INFO,
